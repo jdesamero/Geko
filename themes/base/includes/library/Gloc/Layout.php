c9ab3b09e642bf0e581e5cbeeb395c8798555394
Joel Desamero <joel@geekoracle.com>
2013-08-19 23:41:00 -0400

    getScriptUrls() now handled by super-class

--- themes/base/includes/library/Gloc/Layout.php
+++ themes/base/includes/library/Gloc/Layout.php
@@ -7,24 +7,6 @@ class Gloc_Layout extends Geko_Wp_Layout
 	protected $_sRenderer = 'Gloc_Layout_Renderer';
 	
 	
-	//
-	public function getScriptUrls( $aOther = NULL ) {
-		
-		$oUrl = Geko_Uri::getGlobal();
-		
-		$aRet = array(
-			'curpage' => strval( $oUrl ),
-			'process' => Geko_Uri::getUrl( 'geko_process' ),
-			'url' => get_bloginfo( 'url' )
-		);
-		
-		if ( is_array( $aOther ) ) {
-			$aRet = array_merge( $aRet, $aOther );
-		}
-		
-		return $aRet;
-	}
-	
 	
 }
 
