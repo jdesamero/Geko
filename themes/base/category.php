8a50fc8f880b89ae5d2a34d319d5422a91d914c5
Joel Desamero <joel@geekoracle.com>
2013-08-26 00:03:30 -0400

    Updated the "base" theme

--- themes/base/category.php
+++ themes/base/category.php
@@ -14,7 +14,7 @@ class Gloc_Layout_Template extends Gloc_Layout
 	//
 	public function echoHeadLate() {
 		?>
-		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/styles/pagination.css" />	
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/styles/pagination.css" />
 		<?php
 	}
 	
@@ -30,12 +30,24 @@ class Gloc_Layout_Template extends Gloc_Layout
 		
 		<?php $this->doPagination(); ?>
 		
-		<?php foreach ( $aPosts as $oPost ): ?>
-			<div id="post-<?php $oPost->echoId(); ?>" class="<?php echo $this->applyPostClass(''); ?>">
+		<?php foreach ( $aPosts as $oPost ):
+			
+			//
+			$oImg = $oPost->getAttachments( array(
+				'file_group' => 'main_image'
+			) )->getOne();
+			
+			?><div id="post-<?php $oPost->echoId(); ?>" class="<?php echo $this->applyPostClass(''); ?>">
 				<div class="entry-content">
 					<h2><a href="<?php $oPost->echoUrl(); ?>" title="<?php $this->pw( $this->l_102(), $oPost->escgetTitle() ); ?>" rel="bookmark"><?php $oPost->echoTitle(); ?></a></h2>
 					<p><?php $oPost->echoDateCreated(); ?> - <?php $oPost->echoTimeCreated(); ?></p>
-					<p><?php $oPost->echoTheExcerpt( 300 ); ?></p>
+					<p>
+						<?php if ( $oImg->isValid() ): ?>
+							<img src="<?php $oImg->echoTheImageUrl( array( 'w' => 200, 'h' => 125 ) ); ?>" class="test" />
+						<?php endif; ?>
+						<?php $oPost->echoTheExcerpt( 600 ); ?>
+					</p>
+					<div class="fix"></div>
 					<?php $this->pw( '<p><strong>' . $this->l_101() . '</strong> %s</p>', strval( $oPost->getTags() ) ); ?>
 				</div>
 			</div>
