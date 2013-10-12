e15c8c8409a058e9a383b7c2881adbc8170d3dee
Joel Desamero <joel@geekoracle.com>
2013-10-11 23:56:08 -0400

    Added jquery.scrollingcarousel plugin; Tweaks to Geko_Grid class

--- plugins/geko_core/library/Geko/Grid.php
+++ plugins/geko_core/library/Geko/Grid.php
@@ -9,93 +9,123 @@ class Geko_Grid
 	const GO_ACROSS = 0;
 	const GO_DOWN = 1;
 	
-	private $aSubject;
+	protected $aSubject;
 	
-	private $iLength;
-	private $iCols;
-	private $iRows;
+	protected $iLength;
+	protected $iCols;
+	protected $iRows;
 	
-	private $iBase = self::BASE_ZERO;
-	private $iDirection = self::GO_ACROSS;
+	protected $iBase = self::BASE_ZERO;
+	protected $iDirection = self::GO_ACROSS;
+	
+	protected $bCalculated = FALSE;
+	
 	
 	//
-	public function __construct($aSubject, $iCols)
-	{
-		$this->aSubject = $aSubject;
-		$this->iCols = $iCols;
+	public function __construct( $aSubject, $iCols, $iRows ) {
 		
-		$this->iLength = count($this->aSubject);
-		$this->iRows = ceil($this->iLength / $this->iCols);
+		$this->aSubject = $aSubject;
+		$this->iLength = count( $this->aSubject );
+		
+		$this->iCols = $iCols;
+		$this->iRows = $iRows;
 	}
 	
+	
 	//
-	public function setNumColumns($iCols)
-	{
+	public function setNumColumns( $iCols ) {
 		$this->iCols = $iCols;
 		return $this;
 	}
+
+	//
+	public function setNumRows( $iRows ) {
+		$this->iRows = $iRows;
+		return $this;
+	}
+	
 	
 	//
-	public function setBase($iBase)
-	{
+	public function setBase( $iBase ) {
 		$this->iBase = $iBase;
 		return $this;
 	}
 	
 	//
-	public function setDirection($iDirection)
-	{
+	public function setDirection( $iDirection ) {
 		$this->iDirection = $iDirection;
 		return $this;
 	}
 	
 	//
-	public function cols()
-	{
+	public function cols() {
+		$this->calculate();
 		return $this->iCols;
 	}
 	
 	//
-	public function rows()
-	{
+	public function rows() {
+		$this->calculate();
 		return $this->iRows;
 	}
 	
 	//
-	public function length()
-	{
+	public function length() {
+		$this->calculate();
 		return $this->iLength;
 	}
 	
 	//
-	public function item($i, $j)
-	{
+	public function item( $i, $j ) {
+		
+		$this->calculate();
+		
 		// calculate $k
-		if (self::GO_DOWN == $this->iDirection) {
-			if (self::BASE_ONE == $this->iBase) {
-				$k = ($this->iRows * ($j - 1)) + ($i - 1);
+		if ( self::GO_DOWN == $this->iDirection ) {
+			if ( self::BASE_ONE == $this->iBase ) {
+				$k = ( $this->iRows * ( $j - 1 ) ) + ( $i - 1 );
 			} else {
 				// self default self::BASE_ZERO
-				$k = ($this->iRows * $j) + $i;
+				$k = ( $this->iRows * $j ) + $i;
 			}
 		} else {
 			// default self::GO_ACROSS
-			if (self::BASE_ONE == $this->iBase) {
-				$k = ($this->iCols * ($i - 1)) + ($j - 1);
+			if ( self::BASE_ONE == $this->iBase ) {
+				$k = ( $this->iCols * ( $i - 1 ) ) + ( $j - 1 );
 			} else {
 				// self default self::BASE_ZERO
-				$k = ($this->iCols * $i) + $j;
+				$k = ( $this->iCols * $i ) + $j;
 			}
 		}
 		
 		// return value
-		if (self::BASE_ONE == $this->iBase) {
-			return ($k <= $this->iLength) ? $this->aSubject[$k] : NULL;
+		if ( self::BASE_ONE == $this->iBase ) {
+			return ( $k <= $this->iLength ) ? $this->aSubject[ $k ] : NULL;
 		} else {
 			// self default self::BASE_ZERO
-			return ($k < $this->iLength) ? $this->aSubject[$k] : NULL;
+			return ( $k < $this->iLength ) ? $this->aSubject[ $k ] : NULL;
 		}		
 	}
+	
+	// do once
+	public function calculate() {
+		
+		if ( !$this->bCalculated ) {
+			
+			if ( NULL === $this->iCols ) {
+				$this->iCols = intval( ceil( $this->iLength / $this->iRows ) );		
+			}
+			
+			if ( NULL === $this->iRows ) {
+				$this->iRows = intval( ceil( $this->iLength / $this->iCols ) );		
+			}
+			
+			$this->bCalculated = TRUE;
+		}
+		
+		return $this;
+	}
+	
 	
 }
 
