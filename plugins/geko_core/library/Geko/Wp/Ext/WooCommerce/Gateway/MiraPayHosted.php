0ad63f8971a3c5e5b7b87383b40d20cdf00ca2ea
Joel Desamero <joel@geekoracle.com>
2015-01-31 23:02:56 -0500

    Added WooCommerce MiraPay module

--- plugins/geko_core/library/Geko/Wp/Ext/WooCommerce/Gateway/MiraPayHosted.php
+++ plugins/geko_core/library/Geko/Wp/Ext/WooCommerce/Gateway/MiraPayHosted.php
@@ -7,6 +7,20 @@ class Geko_Wp_Ext_WooCommerce_Gateway_MiraPayHosted extends WC_Payment_Gateway
 	protected $_sStagingGatwayUrl = 'https://staging.eigendev.com/mirapay/secure_credit.php';
 	protected $_sLiveGatwayUrl = 'https://www3.eigendev.com/mirapay/secure_credit.php';
 	
+	protected $_sPreTransHashKey = 'mirapay_pre_transaction_hash';
+	protected $_sPreCheckKey = 'mkeycheck';
+	protected $_sPostTransHashKey = 'mirapay_post_transaction_hash';
+	protected $_sMiraResponseKey = 'mirapay_response';
+	protected $_sMiraIdKey = 'mirapay_transaction_id';
+	protected $_sMiraStatusKey = 'mirapay_status';
+	
+	protected $_aNotices = array(
+		'failed' => 'Your payment failed!',
+		'cancelled' => 'You cancelled your payment!',
+		'key_mismatch' => 'Transaction key mismatch! Please send an email to %s for assistance!',
+		'unknown' => 'An unknown Mirapay status was sent ("%s"). Please send an email to %s and report the error.'
+	);
+	
 	
 	
 	//
@@ -26,10 +40,38 @@ class Geko_Wp_Ext_WooCommerce_Gateway_MiraPayHosted extends WC_Payment_Gateway
 		add_action( sprintf( 'woocommerce_update_options_payment_gateways_%s', $this->id ), array( $this, 'process_admin_options' ) );
 		
 		$this->handleMiraPayResponse();
+		$this->sendNotices();
 		
 	}
 	
 	
+	//// accessors
+	
+	//
+	public function getModePrefix() {
+		
+		$sMode = $this->get_option( 'mode' );
+		
+		return sprintf( '%s_', $sMode );
+	}
+	
+	// apply mode prefix
+	public function getModeOption( $sKey ) {
+		
+		$sPrefix = $this->getModePrefix();
+		
+		return $this->get_option( sprintf( '%s%s', $sPrefix, $sKey ) );
+	}
+	
+	//
+	public function getOrderAmount( $oOrder ) {
+		
+		return number_format( $oOrder->get_total() - round( $oOrder->get_total_shipping() + $oOrder->get_shipping_tax(), 2 ) + $oOrder->get_order_discount(), 2, '.', '' );
+	}
+	
+	
+	
+	//// main methods
 	
 	//
 	public function init_form_fields() {
@@ -76,7 +118,13 @@ class Geko_Wp_Ext_WooCommerce_Gateway_MiraPayHosted extends WC_Payment_Gateway
 			),
 			
 			'staging_password' => array(
-				'title' => __( 'Staging Password Hash', 'woocommerce' ),
+				'title' => __( 'Staging Password', 'woocommerce' ),
+				'type' => 'text',
+				'default' => ''
+			),
+			
+			'staging_order_prefix' => array(
+				'title' => __( 'Staging Order Prefix', 'woocommerce' ),
 				'type' => 'text',
 				'default' => ''
 			),
@@ -96,17 +144,29 @@ class Geko_Wp_Ext_WooCommerce_Gateway_MiraPayHosted extends WC_Payment_Gateway
 			),
 			
 			'live_password' => array(
-				'title' => __( 'Live Password Hash', 'woocommerce' ),
+				'title' => __( 'Live Password', 'woocommerce' ),
 				'type' => 'text',
 				'default' => ''
 			),
 			
+			'live_order_prefix' => array(
+				'title' => __( 'Live Order Prefix', 'woocommerce' ),
+				'type' => 'text',
+				'default' => ''
+			),
 			
-			
-			'description' => array(
+			/* 'description' => array(
 				'title' => __( 'Customer Message', 'woocommerce' ),
 				'type' => 'textarea',
 				'default' => ''
+			) */
+			
+			'support_email' => array(
+				'title' => __( 'Support Email', 'woocommerce' ),
+				'type' => 'text',
+				'description' => __( 'Email used when displaying a "please contact admin" error.', 'woocommerce' ),
+				'default' => get_bloginfo( 'admin_email' ),
+				'desc_tip' => TRUE
 			)
 			
 		);
@@ -116,35 +176,34 @@ class Geko_Wp_Ext_WooCommerce_Gateway_MiraPayHosted extends WC_Payment_Gateway
 	//
 	public function process_payment( $iOrderId ) {
 		
+		global $woocommerce;
+		
+		
 		$oOrder = new WC_Order( $iOrderId );
+		$oCustomer = $oOrder->get_user();
 		
-		$sMode = $this->get_option( 'mode' );
-		$sPrefix = sprintf( '%s_', $sMode );
+		// $sLogin = $this->getModeOption( 'login' );
+		$sMerchantId = $this->getModeOption( 'merchant_id' );
+		$sPassword = $this->getModeOption( 'password' );
+		$sOrderPrefix = $this->getModeOption( 'order_prefix' );
 		
-		$sLogin = $this->get_option( sprintf( '%slogin', $sPrefix ) );
-		$sMerchantId = $this->get_option( sprintf( '%smerchant_id', $sPrefix ) );
-		$sPassword = $this->get_option( sprintf( '%spassword', $sPrefix ) );
+		$sCustomerEmail = $oCustomer->user_email;
+		$sMtId = sprintf( '%s%d', $sOrderPrefix, $iOrderId );
 		
+		$sAmount = $this->getOrderAmount( $oOrder );
 		
+		$sMkey = md5( sprintf( '%s%s%s', $sMtId, $sAmount, $sPassword ) );
 		
-		$sAmount = number_format( $oOrder->get_total() - round( $oOrder->get_total_shipping() + $oOrder->get_shipping_tax(), 2 ) + $oOrder->get_order_discount(), 2, '.', '' );
+		// track the mkey above, and assign as order meta data
 		
-		$sMkey = md5( sprintf( '%s%s%s', $sLogin, $sAmount, $sPassword ) );
+		update_post_meta( $iOrderId, $this->_sPreTransHashKey, $sMkey );
 		
+		// set-up the return URL
+		// SuccessURL and FailURL is the same, use "Response" query string value to handle error situation
+		$oReturnUrl = new Geko_Uri( $woocommerce->cart->get_checkout_url() );
+		$oReturnUrl->setVar( $this->_sPreCheckKey, $sMkey );
 		
-		// set-up the success and failure url
-		
-		$oReturnUrl = new Geko_Uri( sprintf( '%s/store/checkout/', Geko_Wp::getUrl() ) );
-		$oReturnUrl
-			->setVar( 'status', 'success' )
-			->setVar( 'oid', $iOrderId )
-		;
-		
-		$sSuccessUrl = strval( $oReturnUrl );
-		
-		$oReturnUrl->setVar( 'status', 'fail' );
-		
-		$sFailUrl = strval( $oReturnUrl );
+		$sReturnUrl = strval( $oReturnUrl );
 		
 		
 		// set-up the gateway url
@@ -153,14 +212,37 @@ class Geko_Wp_Ext_WooCommerce_Gateway_MiraPayHosted extends WC_Payment_Gateway
 		
 		$oGatewayUrl = new Geko_Uri( $sGatewayUrl );
 		$oGatewayUrl
-			->setVar( 'MTID', $sLogin )
+			->setVar( 'MTID', $sMtId )
 			->setVar( 'Merchant_ID', $sMerchantId )
 			->setVar( 'MKEY', $sMkey )
-			->setVar( 'SuccessURL', $sSuccessUrl )
-			->setVar( 'FailURL', $sFailUrl )
-			->setVar( 'EMail', get_bloginfo( 'admin_email' ) )
+			->setVar( 'SuccessURL', $sReturnUrl )
+			->setVar( 'FailURL', $sReturnUrl )
+			->setVar( 'EMail', $sCustomerEmail )
 			->setVar( 'Amount1', $sAmount )
 		;
+		
+		
+		/* */
+		
+		$sLogFile = '/var/www/vhosts/geekoracle.com/dev.geekoracle.com/emondexamprep/foo.txt';
+		
+		$sOut =
+			"==========================\n" .
+			sprintf( "Date: %s\n\n", date( 'D, j M Y H:i:s' ) ).
+			sprintf( "MTID: %s\n", $sMtId ) . 
+			sprintf( "Merchant_ID: %s\n", $sMerchantId ) . 
+			sprintf( "MKEY: %s\n", $sMkey ) . 
+			sprintf( "SuccessURL: %s\n", $sReturnUrl ) . 
+			sprintf( "FailURL: %s\n", $sReturnUrl ) . 
+			sprintf( "EMail: %s\n", $sCustomerEmail ) . 
+			sprintf( "Amount1: %s\n", $sAmount ) . 
+			"\n\n";
+		;
+		
+		file_put_contents( $sLogFile, $sOut, FILE_APPEND );
+		
+		/* */
+		
 		
 		// Return thankyou redirect
 		return array(
@@ -173,28 +255,140 @@ class Geko_Wp_Ext_WooCommerce_Gateway_MiraPayHosted extends WC_Payment_Gateway
 	//
 	public function handleMiraPayResponse() {
 		
-		$iOrderId = $_GET[ 'oid' ];
-		$sMkey = $_GET[ 'MKEY' ];
+		global $woocommerce;
 		
-		if ( $iOrderId && $sMkey ) {
+		
+		$sMkeyCheck = trim( $_GET[ $this->_sPreCheckKey ] );
+		$sPostMkey = trim( $_GET[ 'MKEY' ] );		// this is the new MKEY
+		$sMtId = trim( $_GET[ 'MTID' ] );
+		$sMiraId = trim( $_GET[ 'MiraID' ] );
+		$sResponse = trim( $_GET[ 'Response' ] );
+		
+		$oRedirectUrl = NULL;
+		
+		//
+		if ( $sMkeyCheck && $sPostMkey && $sMtId && $sMiraId && $sResponse ) {
 			
-			global $woocommerce;
+			// checkout page is the default redirect URL
+			$oRedirectUrl = new Geko_Uri( $woocommerce->cart->get_checkout_url() );
 			
+			
+			// obtain the order id
+			// strip prefix from mtid
+			
+			$sOrderPrefix = $this->getModeOption( 'order_prefix' );
+			$sMerchantId = $this->getModeOption( 'merchant_id' );
+			$sPassword = $this->getModeOption( 'password' );
+			
+			$iOrderId = intval( substr( $sMtId, strlen( $sOrderPrefix ) ) );
+			
+			// validate the order
 			$oOrder = new WC_Order( $iOrderId );
 			
-			// Mark as on-hold (we're awaiting the cheque)
-			$oOrder->update_status( 'complete', __( 'MiraPay Hosted Payment was Completed', 'woocommerce' ) );
+			$sAmount = $this->getOrderAmount( $oOrder );
 			
-			// Reduce stock levels
-			$oOrder->reduce_order_stock();
+			// get the stored mkey (pre)
+			$sStoredMKey = get_post_meta( $iOrderId, $this->_sPreTransHashKey, TRUE );
+			
+			// calculate the new mkey (post)
+			// MTID, Amount1, MiraID, Response and the Merchant's Password
+			$sPostMkeyCheck = md5( sprintf( '%s%s%s%s%s', $sMtId, $sAmount, $sMiraId, $sResponse, $sPassword ) );
+			
+			
+			if (
+				( $sPostMkey == $sPostMkeyCheck ) && 
+				( $sStoredMKey == $sMkeyCheck )
+			) {
+				
+				// stored mkey and given mkey is a match, store transaction details
+				
+				update_post_meta( $iOrderId, $this->_sPostTransHashKey, $sPostMkey );
+				update_post_meta( $iOrderId, $this->_sMiraResponseKey, $sResponse );
+				update_post_meta( $iOrderId, $this->_sMiraIdKey, $sMiraId );
+				
+				
+				// handle responses accordingly
+				
+				if ( 'APPROVED' == $sResponse ) {
+				
+					// Mark as complete
+					$oOrder->update_status( 'complete', __( 'MiraPay Hosted Payment was Completed', 'woocommerce' ) );
+					
+					// Reduce stock levels
+					$oOrder->reduce_order_stock();
+				
+					// Remove cart
+					$woocommerce->cart->empty_cart();
+					
+					// redirect to order complete page
+					$oRedirectUrl = new Geko_Uri( $this->get_return_url( $oOrder ) );
+									
+				} else {
+					
+					// log the failure
+					$sOrderStatus = '';
+					$sOrderStatusMsg = '';
+					
+					if ( 'DECLINED' == $sResponse ) {
+						
+						$sOrderStatus = 'failed';
+						$sOrderStatusMsg = 'MiraPay Hosted Payment was Declined';
+						
+					} elseif ( 'CANCELED' == $sResponse ) {
+						
+						$sOrderStatus = 'cancelled';
+						$sOrderStatusMsg = 'MiraPay Hosted Payment was Cancelled';
+					
+					}
+					
+					// update order status
+					if ( $sOrderStatus && $sOrderStatusMsg ) {
+						$oOrder->update_status( $sOrderStatus, __( $sOrderStatusMsg, 'woocommerce' ) );
+						$oRedirectUrl->setVar( $this->_sMiraStatusKey, $sOrderStatus );
+					}
+					
+				}
+				
+			} else {
+				
+				// mkey mismatch
+				$oRedirectUrl->setVar( $this->_sMiraStatusKey, 'key_mismatch' );
+			}
+			
+		}
 		
-			// Remove cart
-			$woocommerce->cart->empty_cart();
+		// redirect, if needed
+		if ( $oRedirectUrl ) {
 			
-			header( sprintf( 'Location: %s', $this->get_return_url( $oOrder ) ) );
+			header( sprintf( 'Location: %s', strval( $oRedirectUrl ) ) );
 			die();
 		}
 		
+	}
+	
+	
+	//
+	public function sendNotices() {
+		
+		if ( $sStatusKey = trim( $_GET[ $this->_sMiraStatusKey ] ) ) {
+			
+			$sNoticeMsg = $this->_aNotices[ $sStatusKey ];
+			$sSupportEmail = $this->get_option( 'support_email' );
+			
+			$aEmailTag = sprintf( '<a href="mailto:%s">%s</a>', $sSupportEmail, $sSupportEmail );
+			
+			if ( 'key_mismatch' == $sStatusKey ) {
+				$sNoticeMsg = sprintf( $sNoticeMsg, $aEmailTag );
+			}
+			
+			if ( !$sNoticeMsg ) {
+				$sNoticeMsg = sprintf( $this->_aNotices[ 'unknown' ], $sStatusKey, $aEmailTag );
+			}
+			
+			wc_add_notice( sprintf( '%s %s', __( 'Payment error:', 'woothemes' ), $sNoticeMsg ), 'error' );
+		}
+
+	
 	}
 	
 	
