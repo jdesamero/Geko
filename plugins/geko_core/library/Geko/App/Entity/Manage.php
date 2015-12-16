e5b8478de0715a6b7b8e89e46325011c0af07a3f
Joel Desamero <joel@geekoracle.com>
2015-12-16 15:46:24 -0500

    Added open source blurbage

--- plugins/geko_core/library/Geko/App/Entity/Manage.php
+++ plugins/geko_core/library/Geko/App/Entity/Manage.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * "geko_core/library/Geko/App/Entity/Manage.php"
+ * https://github.com/jdesamero/Geko
+ *
+ * Copyright (c) 2013 Joel Desamero.
+ * Licensed under the MIT license.
+ */
 
 //
 class Geko_App_Entity_Manage extends Geko_Singleton_Abstract
--- plugins/geko_core/library/Geko/App/Entity/Query.php
+++ plugins/geko_core/library/Geko/App/Entity/Query.php
@@ -1,5 +1,13 @@
 <?php
+/*
+ * "geko_core/library/Geko/App/Entity/Query.php"
+ * https://github.com/jdesamero/Geko
+ *
+ * Copyright (c) 2013 Joel Desamero.
+ * Licensed under the MIT license.
+ */
 
+//
 abstract class Geko_App_Entity_Query extends Geko_Entity_Query
 {
 	
