417960d3b9f7f33ee7b70731bde777e4dfcfa38a
Joel Desamero <joel@geekoracle.com>
2016-01-26 21:51:37 -0500

    Added setMatchTabletToo() method

--- plugins/geko_core/library/Geko/Wp/Resolver/Path/Mobile.php
+++ plugins/geko_core/library/Geko/Wp/Resolver/Path/Mobile.php
@@ -5,6 +5,16 @@ class Geko_Wp_Resolver_Path_Mobile extends Geko_Wp_Resolver_Path
 {
 	
 	protected $_aPrefixes = array( 'Gloc_Layout_Mobile_', 'Geko_Layout_Mobile_', 'Gloc_Layout_', 'Geko_Layout_' );
+	protected $_bMatchTabletToo = FALSE;
+	
+	//
+	public function setMatchTabletToo( $bMatchTabletToo ) {
+		
+		$this->_bMatchTabletToo = $bMatchTabletToo;
+		
+		return $this;
+	}
+	
 	
 	//
 	public function isMatch() {
@@ -12,10 +22,15 @@ class Geko_Wp_Resolver_Path_Mobile extends Geko_Wp_Resolver_Path
 		// perform device detection
 		$oBrowser = new Geko_Browser();
 		
-		return (
-			$oBrowser->isDevice( Geko_Browser::DEVICE_MOBILE ) || 
-			$_GET[ 'geko_wp_resolver_mobile_test' ]
-		) ? TRUE : FALSE ;
+		$bMatchDevice = NULL;
+		
+		if ( $this->_bMatchTabletToo ) {
+			$bMatchDevice = ( $oBrowser->isDevice( Geko_Browser::DEVICE_MOBILE ) || $oBrowser->isDevice( Geko_Browser::DEVICE_TABLET ) );
+		} else {
+			$bMatchDevice = $oBrowser->isDevice( Geko_Browser::DEVICE_MOBILE );
+		}
+		
+		return ( $bMatchDevice || $_GET[ 'geko_wp_resolver_mobile_test' ] ) ? TRUE : FALSE ;
 	}
 	
 	//
